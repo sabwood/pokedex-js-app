4c3a0136de2a490781a1870e9d701efe2ba1cafc
sabwood <150414048+sabwood@users.noreply.github.com>
2023-12-01 20:18:34 -0800

    added three Pokémon objects
    added three Pokémon objects

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,3 +1,16 @@
-alert('Hello world');
-let favoriteFood = 'Lumpia';
-document.write(favoriteFood);
+let pokemonList = [pokemon1, pokemon2, pokemon3]
+let pokemon1 = {
+    name: 'Beedrill', 
+    height: 100,
+    type: ['grass', 'poison']
+}
+let pokemon2 = {
+    name: 'Swoobat',
+    height: 90,
+    type: ['psychic', 'fighting']
+}
+let pokemon3 = {
+    name: 'Gothorita',
+    height: 70,
+    type: ['psychic', 'fighting']
+}
