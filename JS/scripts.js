f19d1bf429270d05de9b85fee710725c807b7b74
sabwood <150414048+sabwood@users.noreply.github.com>
2023-12-01 20:54:37 -0800

    added comment and updated types
    added comment and updated types

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,4 +1,5 @@
-let pokemonList = [{name: 'Beedrill', height: 100, type: ['grass', 'poison']},
-{name: 'Swoobat', height: 90, type: ['psychic', 'fighting']},
+// Height is measured in cm.
+let pokemonList = [{name: 'Beedrill', height: 100, type: ['bug', 'poison']},
+{name: 'Swoobat', height: 90, type: ['psychic', 'flying']},
 {name: 'Gothorita', height: 70, type: ['psychic', 'fighting']}
 ]
