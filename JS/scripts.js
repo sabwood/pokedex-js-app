fce8ea441ad044d3ba160fb28d4fcc6bc23135e6
sabwood <150414048+sabwood@users.noreply.github.com>
2023-12-04 21:06:59 -0800

    updated pokemonList
    updated pokemonList

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,5 +1,5 @@
 // Height is measured in cm.
 let pokemonList = [{name: 'Beedrill', height: 100, type: ['bug', 'poison']},
-{name: 'Swoobat', height: 90, type: ['psychic', 'flying']},
+{name: 'Serperior', height: 330, type: ['grass', 'poison']},
 {name: 'Gothorita', height: 70, type: ['psychic', 'fighting']}
 ]
