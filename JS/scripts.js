8759967550de7c32fca6ddb02d3601c9cf0c21b1
sabwood <150414048+sabwood@users.noreply.github.com>
2023-12-05 20:56:44 -0800

    added 'for' loop and conditional
    added 'for' loop and conditional

--- JS/scripts.js
+++ JS/scripts.js
@@ -3,3 +3,10 @@ let pokemonList = [{name: 'Beedrill', height: 100, type: ['bug', 'poison']},
 {name: 'Serperior', height: 330, type: ['grass', 'poison']},
 {name: 'Gothorita', height: 70, type: ['psychic', 'fighting']}
 ]
+
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height > 150) // Condition for pokemon with the height > 150cm
+     {document.write(pokemonList[i].name + "  " + "(height:"+ "  " +  pokemonList[i].height + " ) " + "- Wow. That\'s big!"+ "<br>");
+    } else {
+  document.write(pokemonList[i].name + "  " + "(height:"+ "  " +  pokemonList[i].height + " ) " + "<br>");}
+  }
