ed9ebc4f33df863894cbb7ec1adbbddeecaa7da1
sabwood <150414048+sabwood@users.noreply.github.com>
2023-12-07 18:41:56 -0800

    Added forEach() function
    Added forEach() function

--- JS/scripts.js
+++ JS/scripts.js
@@ -4,9 +4,27 @@ let pokemonList = [{name: 'Beedrill', height: 100, type: ['bug', 'poison']},
 {name: 'Gothorita', height: 70, type: ['psychic', 'fighting']}
 ]
 
-for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height > 150) // Condition for pokemon with the height > 150cm
-     {document.write(pokemonList[i].name + "  " + "(height:"+ "  " +  pokemonList[i].height + " ) " + "- Wow. That\'s big!"+ "<br>");
-    } else {
-  document.write(pokemonList[i].name + "  " + "(height:"+ "  " +  pokemonList[i].height + " ) " + "<br>");}
+pokemonList.forEach(function(pokemon){
+  if (pokemon.height > 150) // Condition for pokemon with the height > 150cm
+    {document.write(
+      pokemon.name + 
+      "  " + 
+      "(height:"+ 
+      "  " +  
+      pokemon.height + 
+      " ) " + 
+      "- Wow. That\'s big!"+ 
+      "<br>"
+    );
+  } else {
+    document.write(
+      pokemon.name + 
+      "  " + 
+      "(height:"+ 
+      "  " +  
+      pokemon.height + 
+      " ) " + 
+      "<br>"
+    );
   }
+});
