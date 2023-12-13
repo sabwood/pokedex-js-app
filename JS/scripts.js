327e2596d41c3f95795b66cb27647b122e2510da
sabwood <150414048+sabwood@users.noreply.github.com>
2023-12-13 13:36:22 -0800

    added addListItem, EventListener, and styling
    added addListItem, EventListener, and styling

--- JS/scripts.js
+++ JS/scripts.js
@@ -11,35 +11,36 @@ let pokemonRepository = (function(){
         return pokemonList;
     },
     add: function (item) {
-      pokemonList.push(item);
-    }
+        pokemonList.push(item);
+    },
+    addListItem,
   }
 })();
 
 console.log(pokemonRepository.getAll());
-console.log(pokemonRepository.add({name: 'Swoobat', height: 90, type: ['psychic', 'fighting'] }));
+console.log(pokemonRepository.add({name: 'Swoobat', height: 90, type: ['psychic', 'fighting']}))
+
+function showDetails(pokemon) {
+    console.log(pokemon);
+}
+
+function addListItem(pokemon){
+  let pokemonList = document.querySelector('.pokemon-list');
+  let listItem = document.createElement('li');
+  let button = document.createElement('button');
+
+  button.innerText = pokemon.name;
+  button.classList.add('pokemon-button');
+
+  listItem.appendChild(button);
+  pokemonList.appendChild(listItem);
+  
+  button.addEventListener("click", function () {
+    showDetails(pokemon.name)
+  });
+}
 
 pokemonRepository.getAll().forEach(function(pokemon){
-  if (pokemon.height > 150) // Condition for pokemon with the height > 150cm
-    {document.write(
-      pokemon.name + 
-      "  " + 
-      "(height:"+ 
-      "  " +  
-      pokemon.height + 
-      " ) " + 
-      "- Wow. That\'s big!"+ 
-      "<br>"
-    );
-  } else {
-    document.write(
-      pokemon.name + 
-      "  " + 
-      "(height:"+ 
-      "  " +  
-      pokemon.height + 
-      " ) " + 
-      "<br>"
-    );
+  pokemonRepository.addListItem(pokemon)
   }
-});
+);
